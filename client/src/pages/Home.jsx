fe5f27d82205af2da018e7860072ecd0ed82e9f8
jayadeep217 <gaddamjayadeep@gmail.com>
2026-04-27 12:15:03 +0530

    nav bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,31 +1,91 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "../api/auth.calls.js";
+import { setUserData } from "../redux/userSlice.js";
 
-function HomePage() {
-  const [userData, setUserData] = useState(null);
+import { Layout, Input, Button, Avatar, Typography, Space } from "antd";
+import {
+  UserOutlined,
+  LogoutOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+
+const { Header } = Layout;
+const { Text } = Typography;
+
+function Home() {
+  const userData = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+
+  const fetchUser = async () => {
+    try {
+      const data = await getUser();
+      dispatch(setUserData(data.data));
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const data = await getUser();
-        setUserData(data.data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
     fetchUser();
-  }, []);
+  });
+
+  const onSearch = (value) => {
+    console.log("Search:", value);
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(setUserData(null));
+  };
+
+  const displayName = userData?.name || "Guest";
 
   return (
-    <div className="home-page">
-      <h1>Welcome to ShowHive, {userData?.name || "Guest"}!</h1>
+    <Layout>
+      <Header
+        style={{
+          background: "rgb(235, 78, 98)",
+          display: "flex",
+          alignItems: "center",
+          padding: "0 20px",
+        }}
+      >
+        {/* Logo / Brand */}
+        <Text strong style={{ fontSize: 18 }}>
+          MyApp
+        </Text>
 
-      <p>
-        Your ultimate platform for discovering and booking movies and events.
-      </p>
-    </div>
+        {/* Search Bar */}
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            padding: "0 20px",
+          }}
+        >
+          <Input
+            placeholder="Search..."
+            onPressEnter={(e) => onSearch(e.target.value)}
+            style={{ maxWidth: 400 }}
+            prefix={<SearchOutlined />}
+          />
+        </div>
+
+        {/* User Info + Logout */}
+        <Space>
+          <Avatar icon={<UserOutlined />} />
+          <Text>{displayName}</Text>
+          <Button icon={<LogoutOutlined />} onClick={onLogout} type="default">
+            Logout
+          </Button>
+        </Space>
+      </Header>
+
+      <div style={{ padding: 20 }}>{/* Page content */}</div>
+    </Layout>
   );
 }
 
-export default HomePage;
+export default Home;
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/user.routes.js");
 const cookieParser = require("cookie-parser");
 
 
+
 const app = express();
 
 app.use(express.json());
